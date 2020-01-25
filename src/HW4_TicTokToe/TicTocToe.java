ef49042ead27d021c97c9abeb67f326f3f7a6085
Igorbanza <59967038+Igorbanza@users.noreply.github.com>
2020-01-26 01:50:51 +0300

    Инициализация поля

--- src/HW4_TicTokToe/TicTocToe.java
+++ src/HW4_TicTokToe/TicTocToe.java
@@ -1,5 +1,31 @@
 package HW4_TicTokToe;
 
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTocToe {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random random = new Random();
+
+    public static void main(String[] args) {
+
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
+
+    }
+
 
 }
+
+
