6543cbd2f4346d490cfd19e0d57d8a1450f2eaf8
Igorbanza <59967038+Igorbanza@users.noreply.github.com>
2020-01-27 17:38:10 +0300

    Думаю как же сделать проверку победы=)

--- src/HW4_TicTokToe/TicTocToe.java
+++ src/HW4_TicTokToe/TicTocToe.java
@@ -4,38 +4,67 @@ package HW4_TicTokToe;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TicTocToe {
-    public static int SIZE = 3;
-    public static int DOTS_TO_WIN = 3;
+public class TicTocToe {// Создание двумерного символьного массива размером 3x3/
+    // в каждом масссиве могут находиться символы: '0' 'X' '.'
+    private static char[][] map;
+    private static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+    // именные константы
     public static final char DOT_EMPTY = '•';
     public static final char DOT_X = 'X';
     public static final char DOT_O = 'O';
-    public static char[][] map;
+
     public static Scanner sc = new Scanner(System.in);
-    public static Random random = new Random();
+    public static Random rand = new Random();
 
     public static void main(String[] args) {
+        //Основной игровой цикл
+
         initMap();
         printMap();
-
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Вы победили!");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
     }
 
-    public static void initMap() {  //инициализация поля
+    // Инициализация поля
+    public static void initMap() {
         map = new char[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 map[i][j] = DOT_EMPTY;
             }
         }
+
     }
 
-    public static void printMap() {         //Вывод поля в консоль.
-                                            // Проинициализированное поле готово.
-                                            // Теперь необходимо вывести его в консоль.
+    // Вывод поля в консоль.
+    public static void printMap() {
         for (int i = 0; i <= SIZE; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
+
         for (int i = 0; i < SIZE; i++) {
             System.out.print((i + 1) + " ");
             for (int j = 0; j < SIZE; j++) {
@@ -46,7 +75,64 @@ public class TicTocToe {
         System.out.println();
     }
 
+    // Ход человека.
+
+
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координату по широте");
+            x = sc.nextInt() - 1;
+            System.out.println("Введите координату по высоте:");
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+
+    //isCellValid() проверяет возможность установки фишки в указанную ячейку.
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    //Ход компьютера
+    public static void aiTurn() {
+        int x, y;
+        do {
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
+
+    //Проверка победы.
+    public static boolean checkWin(char symb) {
+        if (map[0][0] == symb && map[0][1] == symb && map[0][2] == symb) return true;
+        if (map[1][0] == symb && map[1][1] == symb && map[1][2] == symb) return true;
+        if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb) return true;
+        if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb) return true;
+        if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb) return true;
+        if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb) return true;
+        if (map[2][0] == symb && map[1][1] == symb && map[0][2] == symb) return true;
+        return false;
+    }
+
+    // isMapFull
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
         }
+        return true;
+    }
+
+}
 
 
 
